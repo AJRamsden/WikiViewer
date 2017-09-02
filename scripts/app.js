be1f7ac10d4a66f59be120aff48c9e4d7d593b8f
unknown <ramsdenaj1@gmail.com>
2017-09-02 22:05:23 +0200

    API Call function

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,25 @@
-var randomArticleURL = 'https://en.wikipedia.org/w/api.php?action=query&format=json&titles=Stack%20Overflow';
-var test1 = [];
+var randomArticleURL = 'https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=cow&origin=*';
+//https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=cow&origin=*';
 $(document).ready(function(){
+  var searchResult = [];
+  var test2 = "";
+  var test3 = "";
+
+  $("#randomArticle").click(function(){
+    $("#wikiArticle").html("");
+    var html = "";
+    pages = [];
+
+    $.getJSON(randomArticleURL, function(response){
+      searchResult = response;
+
+      test2 = searchResult.query.search[0].title;
+      test3 = searchResult.query.search[0].snippet;
 
 
+      $("#wikiArticle").append("<div><h3>" + test2 + "</h3>" + " " + test3);
+      alert(test2);
+    });
+  });
 
 });
-
-function getRandomArticle(url, success){
-  $.getJSON(randomArticleURL, function(response){
-    test1 = response.success;
-  });
-  alert(test1);
-}
